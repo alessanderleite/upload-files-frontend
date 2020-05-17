28fce48d50495390f37e48d28f456ddfbb4a45be
Alessander <alessander.logistica@gmail.com>
2020-05-17 19:54:10 -0300

    ng g c components/upload-files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UploadFilesComponent } from './components/upload-files/upload-files.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadFilesComponent
   ],
   imports: [
     BrowserModule
