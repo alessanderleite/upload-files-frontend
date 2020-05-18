e5dce5cbde464745bc425d8f3b7aa9e1890edeab
Alessander <alessander.logistica@gmail.com>
2020-05-17 22:31:38 -0300

    import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { UploadFilesComponent } from './components/upload-files/upload-files.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { UploadFilesComponent } from './components/upload-files/upload-files.com
     UploadFilesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
